a4b62a21ee02e237e048fb476c02aefdab1a7a62
jiangzihao2009 <452084368@qq.com>
2024-09-21 05:22:27 +0800

    handle markdown

--- project2pdf.py
+++ project2pdf.py
@@ -2,6 +2,15 @@ import os
 import argparse
 import shutil
 import time
+import logging
+import Code2pdf
+import markdown
+from weasyprint import HTML
+
+
+logger = logging.getLogger()
+logger.name = 'repo2pdf>'
+logger.setLevel(logging.INFO)
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-d', '--src_dir', type=str, help="project directory")
@@ -9,63 +18,80 @@ args = parser.parse_args()
 
 input_abspath = os.path.abspath(args.src_dir)
 if not os.path.exists(input_abspath):
-    print(f'invalid input directory: {input_abspath}')
+    logger.info(f'invalid input directory: {input_abspath}')
     exit(0)
 out_dir = input_abspath + '_pdf'
 
 def build_directory(dir_path : str):
     if os.path.exists(dir_path):
-        print(f'delete old path: {dir_path}')
+        logger.info(f'delete old path: {dir_path}')
         shutil.rmtree(dir_path)
 
     try:
         os.mkdir(dir_path)
-        print(f'create directory {dir_path} success')
+        logger.info(f'create directory {dir_path} success')
     except:
-        print(f'create directory {dir_path} failed.')
+        logger.info(f'create directory {dir_path} failed.')
 
 file_black_list = {'.gitignore'}
-file_suffix_black_list = {}
+file_suffix_black_list = {'.js', '.css', '.png', '.svg',
+                          '.jpg', '.pdf', '.toml', '.ps1', '.pb', '.onnx'}
 def is_black_file(file: str):
     if file in file_black_list:
         return True
+    name, file_extension = os.path.splitext(file)
+    if file_extension in file_suffix_black_list:
+        return True
     return False
+
+def markdown2pdf(input_path, out_path):
+    with open(input_path,'r') as f:
+        html = markdown.markdown(f.read(), extensions=['fenced_code', 'codehilite'])
+    HTML(string=html).write_pdf(out_path)
+
 
 def handle_one_file(src_path: str, dst_path: str, file: str):
     filename = dst_path + '/' + file
     if is_black_file(file):
-        print(f'skip file {filename}')
+        logger.info(f'skip file {filename}')
         return False
 
     src_filename = src_path + '/' + file
     dst_filename = filename + '.pdf'
-    print(f'generate pdf file {dst_filename}')
+    name, file_extension = os.path.splitext(file)
+    logger.info(f'generate pdf file {dst_filename}')
     try:
-        os.system(f'code2pdf -l {src_filename} {dst_filename}')
-    except:
-        print(f'fail, generate {dst_filename}')
+        args = {'filename':src_filename, 'outputfile':dst_filename,
+                'linenos': True, 'size':'A4', 'style':'xcode'}
+        if file_extension == '.md':
+            markdown2pdf(src_filename, dst_filename)
+        else:
+            Code2pdf.main(args)
+        #os.system(f'code2pdf -l {src_filename} {dst_filename}')
+    except Exception as e:
+        logger.info(f'convert {src_filename} failed, err: {e}')
         return False
     return True
 
 def handle_one_dir(src_path: str, dst_path:str, files:list[str]):
     if not os.path.exists(dst_path):
-        print(f'path: {dst_path} does not exit, fail.')
+        logger.info(f'path: {dst_path} does not exit, fail.')
         return 0
     cnt = 0
     for file in files:
         if handle_one_file(src_path, dst_path, file):
             cnt += 1
-    print(f'HANDLE {cnt}/{len(files)} files in path({dst_path})')
+    logger.info(f'HANDLE {cnt}/{len(files)} files in path({dst_path})')
     return cnt
 
 def map_absdir(path: str, src_root: str, dst_root: str):
     src_root_len = len(src_root)
     if path[:src_root_len] != src_root:
-        print(f'Invalid path map, path:{path}, src root: {src_root}, dst root: {dst_root}')
+        logger.info(f'Invalid path map, path:{path}, src root: {src_root}, dst root: {dst_root}')
         return None
     return dst_root + path[src_root_len:]
 
-dir_black_list = {'.git'}
+dir_black_list = {'.git', 'css'}
 def is_black_path(path:str):
     base_name = os.path.basename(path)
     dir_name = os.path.dirname(path)
@@ -83,7 +109,7 @@ if __name__ == '__main__':
     for path, _, files in os.walk(input_abspath):
         abs_path= os.path.abspath(path)
         if is_black_path(abs_path):
-            print(f'skip path:{abs_path}')
+            logger.info(f'skip path:{abs_path}')
             dir_black_list.add(abs_path)
             continue
 
@@ -95,6 +121,6 @@ if __name__ == '__main__':
             build_directory(dst_dir)
 
         file_cnt += handle_one_dir(abs_path, dst_dir, files)
-    print(f'create directory: {dir_cnt}, convert files: {file_cnt}')
+    logger.info(f'create directory: {dir_cnt}, convert files: {file_cnt}')
     cost = time.time() - start
-    print(f'cost time: {cost} seconds.')
+    logger.info(f'cost time: {cost} seconds.')
